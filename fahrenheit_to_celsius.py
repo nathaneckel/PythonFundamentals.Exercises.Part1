380bd39e6f99101d8a58f44ab0cb573efd6fd588
nathaneckel <nathan@patientpaperwork.com>
2024-03-13 13:14:50 -0400

    made improvements

---
+++ fahrenheit_to_celsius.py
@@ -0,0 +1,14 @@
+#ToImprove: limit decimals to 2 decimals!
+
+#declaring the input variable fahrenheit
+fahrenheit = float(input("Enter the Temperature in F"))
+
+#declaring the celsius variable
+celsius = (fahrenheit / 1.8) -32
+
+#round(celsius)
+#not yet successfully limiting to 2 decimals
+print(str(fahrenheit) + "degrees fahrenheit is equal to " + str(celsius) + "degrees celsius.")
+
+
+
---
+++ first_name_500_times.py
@@ -0,0 +1,4 @@
+first_name = "Nathan"
+for i in range(501):
+	print("Nathan", i)
+
